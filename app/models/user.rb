55ea78219527b2874ff3055fd59a24afdf5e013e
Zacharia <ndunguzachary24@gmail.com>
2022-12-25 11:35:55 +0300

    validating user inputs

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    has_secure_password
+    validates :first_name, presence: true,  uniqueness:  true
+    validates :second_name, presence: true,  :uniqueness: true
+    validates  :username, presence:  true, :uniqueness: true 
+    validates :email, presence: true, uniqueness: true
+    validates :email, format: {with:  URI::MailTo::EMAIL_REGEXP}
+    validates :password_digest, length: {minimum: 6}, if: ->{new_record? || !password_digest.nill?}
+end
